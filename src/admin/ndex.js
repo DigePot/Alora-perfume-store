0e0a738e2cdb89f3fdd78efbeca0f216574d834d
DigePot <digepot@gmail.com>
2025-08-05 15:51:31 +0300

    Delete src directory

--- src/admin/ndex.js
+++
@@ -1,7 +0,0 @@
-// src/pages/admin/index.js
-
-export { default as Dashboard } from "./Dashboard";
-export { default as Products } from "./Products";
-export { default as Orders } from "./Orders";
-export { default as Users } from "./Users";
-export { default as Settings } from "./Settings";
--- src/pages/Cart.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Cart() {
-  return <div>🛒 Cart Page - Add/remove items + show total price</div>;
-}
